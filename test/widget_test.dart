7800ce953330acbee64a880938d09206405f0b3d
Ngha-Boris <borisngha6@gmail.com>
2026-06-22 14:22:37 +0100

    add configuration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,8 +4,22 @@ import 'package:house_finder/main.dart';
 void main() {
   testWidgets('App launches and shows welcome text', (WidgetTester tester) async {
     await tester.pumpWidget(const HouseFinderApp());
+    await tester.pumpAndSettle();
 
     expect(find.text('House Finder'), findsOneWidget);
     expect(find.text('Welcome to House Finder'), findsOneWidget);
+  });
+
+  testWidgets('Navigation to House Details works', (WidgetTester tester) async {
+    await tester.pumpWidget(const HouseFinderApp());
+    await tester.pumpAndSettle();
+
+    expect(find.text('Welcome to House Finder'), findsOneWidget);
+
+    await tester.tap(find.text('Get Started'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('House Details'), findsOneWidget);
+    expect(find.text('House #1'), findsOneWidget);
   });
 }
